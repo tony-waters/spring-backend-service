df2d6f6126e8bffab4bbf09a6549bc6b3937bf31
tony-waters <2tonywaters@gmail.com>
2026-01-07 22:35:41 +0000

    add services

---
+++ src/main/java/uk/bit1/spring_jpa_relationships/service/OrderFulfillmentService.java
@@ -0,0 +1,12 @@
+package uk.bit1.spring_jpa_relationships.service;
+
+import uk.bit1.spring_jpa_relationships.entity.Order;
+
+import java.util.List;
+
+public class OrderFulfillmentService {
+
+    public List<Order> getUnfulfilledOrders() {
+        return null;
+    }
+}
---
+++ src/main/java/uk/bit1/spring_jpa_relationships/service/ReportService.java
@@ -0,0 +1,19 @@
+package uk.bit1.spring_jpa_relationships.service;
+
+import uk.bit1.spring_jpa_relationships.entity.Product;
+import uk.bit1.spring_jpa_relationships.repository.CustomerRepository;
+
+public class ReportService {
+
+    private CustomerRepository customerRepository;
+
+    public Product getMostPopularProduct() {
+        return null;
+    }
+
+    public int getCustomerCount() {
+//        customerRepository.
+        return 0;
+    }
+
+}
